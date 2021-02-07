7f806c2f65b7486698e2d2098641d19d7310249b
Daniela <dmuniz@homex.com>
2021-02-07 15:44:47 -0500

    chore: update email definitions

--- src/isEmail.ts
+++ src/isEmail.ts
@@ -1,5 +1,10 @@
 const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
+/**
+ * Checks if value is a valid email
+ *
+ * @param value
+ */
 export const isEmail = (value: any) => {
   return emailRegex.test(value)
 }
