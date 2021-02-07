614521d8e8f6239fa1f8d749141457c34645a350
Daniela <dmuniz@homex.com>
2021-02-07 15:44:47 -0500

    chore: update email definitions

--- src/__tests__/isEmail.spec.ts
+++ src/__tests__/isEmail.spec.ts
@@ -7,17 +7,17 @@ import {
 } from '../__mocks__/mockValues'
 
 describe('src/isEmail', () => {
-  test('Should return true when value is an email', () => {
+  test('Should return true when value is a valid email', () => {
     expect(isEmail(MOCK_EMAIL)).toBe(true)
   })
 
-  test('Should return false when value is not a number', () => {
+  test('Should return false when value is not a valid email', () => {
+    const invalidEmails = ['xx@.com', '@xx.com', '@.com', 'xx@xx', 'x@x.x']
+
     expect(isEmail(MOCK_NULL)).toBe(false)
     expect(isEmail(MOCK_UNDEFINED)).toBe(false)
     expect(isEmail(MOCK_STRING)).toBe(false)
-    expect(isEmail('email@.com')).toBe(false)
-    expect(isEmail('@mail.com')).toBe(false)
-    expect(isEmail('@.com')).toBe(false)
-    expect(isEmail('email@mail')).toBe(false)
+
+    invalidEmails.forEach((email) => expect(isEmail(email)).toBe(false))
   })
 })
