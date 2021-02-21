59ba787817651481b909941bcadb6f23a1b10e8e
Daniela <dmuniz@homex.com>
2021-02-21 15:36:46 -0500

    chore: Update config for build

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,13 @@
-import path from 'path'
-
 import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import url from '@rollup/plugin-url'
-import glob from 'glob'
 import babel from 'rollup-plugin-babel'
 import external from 'rollup-plugin-peer-deps-external'
 import typescript from 'rollup-plugin-typescript2'
 
-const ROOT_DIRECTORY = 'src'
-const ROOT_FILE_PATH = `${ROOT_DIRECTORY}/index.ts`
-const INDEX_FILES = glob.sync(`${ROOT_DIRECTORY}/**/index.ts`)
-
-const input = INDEX_FILES.reduce((componentMap, currentPath) => {
-  const currentPathSplit = currentPath.split(path.sep)
-  const componentName =
-    currentPath === ROOT_FILE_PATH
-      ? 'index'
-      : currentPathSplit[currentPathSplit.length - 2]
-
-  if (!componentName) {
-    return componentMap
-  }
-
-  return {
-    ...componentMap,
-    [componentName]: currentPath,
-  }
-}, {})
-
 export default {
   cache: false,
-  input,
+  input: 'src/index.ts',
   output: {
     dir: 'dist',
     format: 'es',
@@ -42,7 +18,7 @@ export default {
     }),
     babel({
       exclude: /node_modules/,
-      externalHelpers: true, // Exclude babel helpers.
+      externalHelpers: true,
       plugins: ['external-helpers'],
     }),
     json(),
